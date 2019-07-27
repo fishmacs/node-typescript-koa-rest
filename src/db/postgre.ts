be64cebc229f89af19f1d3dafd1d49b67e626979
zw <wzhao1984@gmail.com>
2019-07-27 22:24:59 +0800

    add postgresql module

---
+++ src/db/postgre.ts
@@ -0,0 +1,16 @@
+// {
+//   type: 'postgres',
+//   host: connectionOptions.host,
+//   port: connectionOptions.port,
+//   username: connectionOptions.user,
+//   password: connectionOptions.password,
+//   database: connectionOptions.database,
+//   synchronize: true,
+//   logging: false,
+//   entities: [
+//      'dist/entity/**/*.js'
+//   ],
+//   extra: {
+//       ssl: config.dbsslconn, // if not development, will use SSL
+//   }
+// }
